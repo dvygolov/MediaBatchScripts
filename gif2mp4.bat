3d00794462e773a14cd4a16a2927e4c36b212bd4
Daniel Vygolov <rattack@gmail.com>
2023-09-18 17:44:59 +0400

    Modified Gif2Mp4

--- gif2mp4.bat
+++ gif2mp4.bat
@@ -20,11 +20,11 @@ echo.
 IF "%1"=="" (
 	echo "Converting Gif2Mp4 - whole directory..."
 	FOR /F "tokens=*" %%G IN ('dir /b *.gif') DO (
-		ffmpeg -i "%%G" -b:v 0 -crf 25 -f mp4 -vcodec libx264 -pix_fmt yuv420p "%~nG.mp4"
+		ffmpeg -i "%%G" -crf 25 "%%~nG.mp4"
 	)
 	echo "Conversion complete!"
 ) ELSE (
 	echo "Converting Gif2Mp4 - single file..."
-	ffmpeg -i "%1" -b:v 0 -crf 25 -f mp4 -vcodec libx264 -pix_fmt yuv420p "%~n1.mp4"
+	ffmpeg -i "%1" -crf 25 "%~n1.mp4"
 	echo "Conversion complete!"
 )
